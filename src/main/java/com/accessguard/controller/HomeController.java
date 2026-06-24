c74982b69916e35e5f2e72db271be295df65fff1
Ayush Srivastava <ayush.blank002@gmail.com>
2026-06-24 13:23:00 +0530

    Add health endpoint

---
+++ src/main/java/com/accessguard/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.accessguard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "AccessGuard API is running";
+    }
+}
